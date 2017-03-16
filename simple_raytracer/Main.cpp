291b9e2e92f9eef85b3250e6fb7b46f711a8d023
Joao73844 <joaorego41@gmail.com>
2017-03-16 14:02:28 +0000

    JR_RayTracerTutorial_PartVI Rays_&_Intersections Main.cpp: * Rays, Container with the Intersections, Container for the scene Objects * Object Intersection Loop

--- simple_raytracer/Main.cpp
+++ simple_raytracer/Main.cpp
@@ -3,8 +3,13 @@
 int main(int argc, char * argv[]) {
 	std::cout << "rendering..." << std::endl;
 
-	int n = _WIDTH_ * _HEIGHT_;
+	// Window Variables
+	int width = _WIDTH_;
+	int height = _HEIGHT_;
+	int n = width * height;
+	double aspectratio = (double)width / (double)height;
 	RGBType *pixels = new RGBType[n];
+
 	// Special Vectors/Points
 	/// Origin Point
 	Vect O (0, 0, 0);
@@ -33,7 +38,7 @@ int main(int argc, char * argv[]) {
 	/// Light Colors
 	Color white(1.0, 1.0, 1.0, 1.0);
 	Color pretty_green(0.5, 1.0, 0.5, 0.3);
-	Color gray();
+	Color gray(0.5,0.5,0.5,0);
 	Color black(0.0, 0.0, 0.0, 0.0);
 	Color maroon(0.5, 0.25, 0.25, 0);
 	/// Light Position
@@ -46,18 +51,63 @@ int main(int argc, char * argv[]) {
 	Sphere scene_sphere(O, 1.0, pretty_green);
 	/// Plane
 	Plane scene_plane(Y, -1, maroon);
+	/// Vector (Array) with all the scene objects
+	std::vector<Object3D*> scene_objects;
+	scene_objects.push_back(dynamic_cast<Object3D*>(&scene_sphere));
+	scene_objects.push_back(dynamic_cast<Object3D*>(&scene_plane));
+
+	// Auxiliar Variables
+	///@xamnt @yamnt values that will be slightly to the left
+	/// and slightly to the right, of the direction the camera is pointing
+	double xamnt, yamnt;
 
 	// Ray Tracing Algorithm
-	for (int x = 0; x < _WIDTH_ ; x++) {
-		for (int y = 0; y < _HEIGHT_ ; y++) {
-			thisone = y* _WIDTH_ + x;
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			thisone = y* width + x;
 
+			/// Without AntiAliasing
+			if (width > height) {
+				/// image is wider than it is tall
+				xamnt = ((x + 0.5) / width)*aspectratio - (((width - height) / (double)height) / 2);
+				yamnt = ((height - y) + 0.5) / height;
+			}
+			else if (height > width) {
+				/// image is taller than it is wide
+				xamnt = (x + 0.5) / width;
+				yamnt = (((height - y) + 0.5) / height) / aspectratio - (((height - width) / (double)width) / 2);
+			}
+			else {
+				/// the image is square
+				xamnt = (x + 0.5) / width;
+				yamnt = ((height - y) + 0.5) / height;
+			}
+
+			// Rays
+			/// Ray coordinates
+			/// The Ray will be shoot from the camera, so it
+			/// has to come from the same point
+			Vect cam_ray_origin = scene_cam.getCameraPosition();
+			Vect cam_ray_direction = camdir.vectAdd(camright.vectMul(xamnt - 0.5).vectAdd(camdown.vectMul(yamnt - 0.5))).normalize();
+			Ray cam_ray(cam_ray_origin, cam_ray_direction);
+
+			// Check the intersections
+			/// vector containing the result of all the intersection operations
+			std::vector <double> intersections;
+			/// check if the ray intersects any of the objects in the scene
+			for (unsigned int index = 0; index < scene_objects.size(); index++) {
+				intersections.push_back(scene_objects.at(index)->findIntersection(cam_ray));
+			}
+
+			// Pixel Color
+			/// If its inside the coloring area
 			if ((x > 200 && x < 440) && (y > 200 && y < 280)) {
 				pixels[thisone].r = 23;
 				pixels[thisone].g = 222;
 				pixels[thisone].b = 10;
 			}
 			else {
+				/// Its in the margin
 				pixels[thisone].r = 0;
 				pixels[thisone].g = 0;
 				pixels[thisone].b = 0;
